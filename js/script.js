169d178638a61fb053715e76eb832c1a25a9b51e
Maria <kholod.maria.odessa@gmail.com>
2023-02-25 23:53:09 +0300

    Addition, Subtraction, Multiplication and Division in JS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+let FirstNumber = prompt('Будь ласка, введіть перше число:', 100);
+let SecondNumber = prompt('Будь ласка, введіть перше число:', 100);
+let Addition = Number(FirstNumber)+Number(SecondNumber);
+let Subtraction = Number(FirstNumber)-Number(SecondNumber);
+let Multiplication = Number(FirstNumber)*Number(SecondNumber);
+let Division = Number(FirstNumber)/Number(SecondNumber);
+alert(' Сума '+ Addition +'\n Різниця '+ Subtraction +'\n Множення '+ Multiplication + '\n Ділення '+ Division);
+
+/*
+number FirstNumber = prompt('Будь ласка, введіть перше число:', 100);
+number SecondNumber = prompt('Будь ласка, введіть перше число:', 100);
+number Addition = Number(FirstNumber)+Number(SecondNumber);
+number Subtraction = FirstNumber-SecondNumber;
+number Multiplication = FirstNumber*SecondNumber;
+number Division = FirstNumber/SecondNumber;
+alert(' Сума '+ Addition +'\n Різниця '+ Subtraction +'\n Множення '+ Multiplication + '\n Ділення '+ Division);
+*/
