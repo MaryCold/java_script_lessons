0389414bc4337d89d04bddd6f70b086816c68710
Maria <kholod.maria.odessa@gmail.com>
2023-03-05 21:27:33 +0300

    arrays and loops

--- js/script.js
+++ js/script.js
@@ -1,33 +1,74 @@
+function showResult(mes) {
+    const result = document.querySelector('.result');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `${mes}<br/>`;
+    result.appendChild(newElement);
+  }
 
-let FirstNumber = prompt('Будь ласка, введіть перше число:', 100);
-let SecondNumber = prompt('Будь ласка, введіть перше число:', 100);
-let Subtraction, Division;
-
-if (FirstNumber=='' || SecondNumber=='' || Number.isNaN(Number(FirstNumber)) || Number.isNaN(Number(SecondNumber))){
-    alert('Потрібно вводити числа');
-}
-
-else {
-    Subtraction=Number(FirstNumber)-Number(SecondNumber);
-    if(Number(FirstNumber)<Number(SecondNumber)){
-        let decision = confirm('Перше число меньше другого. Ви впевнені, що хочете продовжити операцію?')
-        if(decision){
-            alert('Результат віднімання:'+ Subtraction);
-        }
-        else{
-            Subtraction='Перше число меньше другого';
+function myMin(arr){
+    let al = Number(arr.length);
+    let minimum = arr[al-1];
+    while (al--){
+        if(isFinite(arr[al]) && arr[al] < minimum){
+            minimum = arr[al];
         }
     }
-    if(Number(SecondNumber)=='0'){
-        Division='Друге число дорівнює нулю';
+    return minimum;
+};
+
+function myMax(arr){
+    let al = Number(arr.length);
+    let maximum = arr[al-1];
+    while (al--){
+        if(isFinite(arr[al]) && arr[al] > maximum){
+            maximum = arr[al];
+        }
+    }
+    return maximum;
+};
+
+
+
+let randomNumberArr = [];
+let displayArr = ' ';
+let sumArr = 0;
+
+for (let i = 0; i < 10; i++) {
+    randomNumberArr[i] = Math.floor(Math.random() * 100);
+    if(i!=0){
+        displayArr+=', ' + randomNumberArr[i];
     }
     else{
-        Division=Number(FirstNumber)/Number(SecondNumber);
+        displayArr+=randomNumberArr[i];
     }
-
-    alert(' Результат додавання: ' + (Number(FirstNumber)+Number(SecondNumber)) + '\n Результат віднімання:' + Subtraction + '\n Результат множення:' + (Number(FirstNumber)*Number(SecondNumber)) + '\n Результат ділення:' + Division);
-    
 }
+displayArr+='\n';
+showResult('Масив складається з:' + displayArr);
+
+
+//randomNumberArr [Math.floor(Math.random() * 10)] = 'елемент масиву, що не є числом';
+
+for (let i = 0; i < 10; i++) {
+    if(isFinite(randomNumberArr[i])){
+        sumArr=Number(sumArr)+Number(randomNumberArr[i]);
+    }
+}
+showResult('Сума елементів масиву: ' + sumArr);
+
+showResult('Мінімальний елемент масиву: ' + myMin(randomNumberArr));
+showResult('Максимальний елемент масиву: ' + myMax(randomNumberArr));
+
+let lines = Math.floor(Math.random() * 12+5); 
+let elem = 1;
+let mes='';
+
+for(let i = 0; i < lines; i++){
+    for(let j = 0; j < i; j++){
+        mes+="#";
+    }
+    mes+="<br/>"
+}
+showResult(mes);
 
 
 
