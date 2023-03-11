5a914311f045e7409b4bb9e6fc021f125b5f5537
Maria <kholod.maria.odessa@gmail.com>
2023-03-11 23:11:14 +0300

    JS Objects Lesson 25

--- js/script.js
+++ js/script.js
@@ -1,99 +1,65 @@
-function showResult(mes) {
-    const result = document.querySelector('.result');
-    const newElement = document.createElement('div');
-    newElement.innerHTML = `${mes}<br/>`;
-    result.appendChild(newElement);
-  }
 
-function myMin(arr){
-    let al = Number(arr.length);
-    let minimum = arr[al-1];
-    while (al--){
-        if(isFinite(arr[al]) && arr[al] < minimum){
-            minimum = arr[al];
+/////////////////   1   //////////////////////////////////////////////
+
+function IsEmpty(object){
+    for(let key in object){
+        if(object.hasOwnProperty(key)){
+            return false;
         }
     }
-    return minimum;
-};
-
-function myMax(arr){
-    let al = Number(arr.length);
-    let maximum = arr[al-1];
-    while (al--){
-        if(isFinite(arr[al]) && arr[al] > maximum){
-            maximum = arr[al];
-        }
-    }
-    return maximum;
-};
-
-function mySubtraction(FirstNumber,SecondNumber){
-    if(Number(FirstNumber)<Number(SecondNumber)){
-        let decision = confirm('Перше число меньше другого. Ви впевнені, що хочете продовжити операцію?')
-        if(decision){
-            alert('Результат віднімання:'+ (Number(FirstNumber)-Number(SecondNumber)));
-            return (Number(FirstNumber)-Number(SecondNumber));
-        }
-        else{
-            return 'Перше число меньше другого';
-        }
-    }
-}
-function myDivision(FirstNumber,SecondNumber){
-    if(Number(SecondNumber)=='0'){
-        return 'Друге число дорівнює нулю';
-    }
-    else{
-        return (Number(FirstNumber)/Number(SecondNumber));
-    }
-
+    return true;
 }
 
+const emptyUser = {};
+const notEmptyUser = {firstName:'Viktor', surname:'Kondol0vskiy'};
+alert(IsEmpty(emptyUser));
+alert(IsEmpty(notEmptyUser));
 
-showResult('<strong>Перша частина завдання</strong>');
-let FirstNumber = prompt('Будь ласка, введіть перше число:', 100);
-let SecondNumber = prompt('Будь ласка, введіть перше число:', 100);
-let Subtraction, Division;
 
-if (FirstNumber=='' || SecondNumber=='' || Number.isNaN(Number(FirstNumber)) || Number.isNaN(Number(SecondNumber))){
-    alert('Потрібно вводити числа');
-}
+/////////////////   2   //////////////////////////////////////////////
 
-else {
-    showResult('Результат додавання:'+ (Number(FirstNumber)+Number(SecondNumber)));
-    showResult('Результат віднімання:' + mySubtraction(FirstNumber,SecondNumber));
-    showResult('Результат множення:' + Number(FirstNumber)*Number(SecondNumber));
-    showResult('Результат ділення:' + myDivision(FirstNumber,SecondNumber));
+function createUser(name,age){
+    return{
+        name: name,
+        age: age,
 
+        sayHello: function(){
+            return  `Привіт, я ${this.name}, мені ${this.age} років`;
+        }
+    };
     
 }
 
-showResult('<strong><br/>Друга частина завдання</strong>');
+const newUser = createUser (prompt('Будь ласка, введіть ваше ім\'я:', 'ім\'я'),prompt('Будь ласка, введіть ваш вік:', 18));
+alert(newUser.sayHello());
 
-let randomNumberArr = [];
-let displayArr = ' ';
-let sumArr = 0;
 
-for (let i = 0; i < 10; i++) {
-    randomNumberArr[i] = Math.floor(Math.random() * 100);
-    if(i!=0){
-        displayArr+=', ' + randomNumberArr[i];
-    }
-    else{
-        displayArr+=randomNumberArr[i];
-    }
+/////////////////   3   //////////////////////////////////////////////
+
+function createCalc(){
+    return{
+        firstNumber:'',
+        secondNumber:'',
+        ask: function(){
+            this.firstNumber = prompt('Будь ласка, введіть перше число:', 100);
+            this.secondNumber = prompt('Будь ласка, введіть перше число:', 100);
+            if (this.firstNumber=='' || this.secondNumber=='' || Number.isNaN(Number(this.firstNumber)) || Number.isNaN(Number(this.secondNumber))){
+                return alert('Потрібно вводити числа');
+            }
+            return this.firstNumber, this.secondNumber;
+        },
+        sum: function(){
+            return (Number(this.firstNumber)+Number(this.secondNumber));
+        },
+        mul: function(){
+            return (Number(this.firstNumber)*Number(this.secondNumber));
+        }
+    };
 }
-displayArr+='\n';
-showResult('Масив складається з:' + displayArr);
 
 
-//randomNumberArr [Math.floor(Math.random() * 10)] = 'елемент масиву, що не є числом';
-
-showResult('Мінімальний елемент масиву: ' + myMin(randomNumberArr));
-showResult('Максимальний елемент масиву: ' + myMax(randomNumberArr));
-
-
-
-
-
+const newCalc = createCalc();
+newCalc.ask();
+alert(`Результат додавання: ${newCalc.sum()}`);
+alert(`Результат множення: ${newCalc.mul()}`);
 
